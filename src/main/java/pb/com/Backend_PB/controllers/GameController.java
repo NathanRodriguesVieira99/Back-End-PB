190f98ee76eed8a0cb7be9fad29d5cbfff7d93fb
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-28 15:05:51 -0300

    feat: implement GameController with Create and List operations

--- src/main/java/pb/com/Backend_PB/controllers/GameController.java
+++ src/main/java/pb/com/Backend_PB/controllers/GameController.java
@@ -1,4 +1,33 @@
 package pb.com.Backend_PB.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pb.com.Backend_PB.entities.Game;
+import pb.com.Backend_PB.useCases.GameUseCases;
+
+@RestController
+@RequestMapping("/game")
 public class GameController {
+  private final GameUseCases gameUseCases;
+
+  public GameController(GameUseCases gameUseCases) {
+    this.gameUseCases = gameUseCases;
+  };
+
+  @GetMapping
+  public List<Game> getAll() {
+    return gameUseCases.getAll();
+  }
+
+  @PostMapping
+  public Game create(@RequestBody Game game) {
+    return gameUseCases.save(game);
+  }
+
 }
