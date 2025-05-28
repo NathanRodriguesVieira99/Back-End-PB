44a41be51c6ad7457be6d4ae7f08991ec3b36596
Nathan Rodrigues Vieira <nathan.vieira@al.infnet.edu.br>
2025-05-28 12:13:49 -0300

    feat: first commit

---
+++ src/main/java/pb/com/Backend_PB/BackendPbApplication.java
@@ -0,0 +1,13 @@
+package pb.com.Backend_PB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pb/com/Backend_PB/BackendPbApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.com.Backend_PB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
