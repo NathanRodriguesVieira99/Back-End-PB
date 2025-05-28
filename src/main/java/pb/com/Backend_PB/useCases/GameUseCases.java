4133a6e7d7920b028b416b71fc264aac3027e615
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-28 15:06:58 -0300

    feat: implement GameUseCases service with methods to get all games and create a game

--- src/main/java/pb/com/Backend_PB/useCases/GameUseCases.java
+++ src/main/java/pb/com/Backend_PB/useCases/GameUseCases.java
@@ -1,4 +1,25 @@
 package pb.com.Backend_PB.useCases;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import pb.com.Backend_PB.entities.Game;
+import pb.com.Backend_PB.repositories.GameRepository;
+
+@Service
 public class GameUseCases {
+  private final GameRepository gameRepository;
+
+  public GameUseCases(GameRepository gameRepository) {
+    this.gameRepository = gameRepository;
+  }
+
+  public List<Game> getAll() {
+    return gameRepository.findAll();
+  }
+
+  public Game save(Game game) {
+    return gameRepository.save(game);
+  }
 }
