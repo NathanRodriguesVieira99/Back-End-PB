715f05df7fb40caaaf44d39a87172df4e3fea852
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 14:23:20 -0300

    feat: add findByCategory method to GameRepository

--- src/main/java/pb/com/Backend_PB/repositories/GameRepository.java
+++ src/main/java/pb/com/Backend_PB/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package pb.com.Backend_PB.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pb.com.Backend_PB.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
+  List<Game> findByCategory(String category);
 }
