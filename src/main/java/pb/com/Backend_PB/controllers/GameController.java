a35acce380eb4322d3b585f388e580f03971a232
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 14:22:47 -0300

    feat: add findByCategory endpoint to GameController

--- src/main/java/pb/com/Backend_PB/controllers/GameController.java
+++ src/main/java/pb/com/Backend_PB/controllers/GameController.java
@@ -2,8 +2,10 @@ package pb.com.Backend_PB.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,12 @@ public class GameController {
   @GetMapping
   public List<Game> getAll() {
     return gameUseCases.getAll();
+  }
+
+  @GetMapping(value = "/category/{category}")
+  public ResponseEntity<List<Game>> findByCategory(@PathVariable String category) {
+    List<Game> list = gameUseCases.findByCategory(category);
+    return ResponseEntity.ok().body(list);
   }
 
   @PostMapping
