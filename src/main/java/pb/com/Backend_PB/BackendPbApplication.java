d36bb91390bd62aa99d48cad56f63623afeed658
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 11:32:32 -0300

    feat: add server running log

--- src/main/java/pb/com/Backend_PB/BackendPbApplication.java
+++ src/main/java/pb/com/Backend_PB/BackendPbApplication.java
@@ -8,6 +8,7 @@ public class BackendPbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendPbApplication.class, args);
+		System.out.println("Server is running on http://localhost:8080 ");
 	}
 
 }
