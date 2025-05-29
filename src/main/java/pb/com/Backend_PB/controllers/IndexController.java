59fe130c823c259797a29ba7428e877596959988
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-29 11:33:10 -0300

    feat: create IndexController with / route returning 'ok'

---
+++ src/main/java/pb/com/Backend_PB/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package pb.com.Backend_PB.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+  @RequestMapping("/")
+  @GetMapping
+  public String mainRoute() {
+    return "ok";
+  }
+
+}
