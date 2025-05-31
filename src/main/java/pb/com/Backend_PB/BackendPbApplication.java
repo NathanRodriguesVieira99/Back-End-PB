9599b87e78138ac3bfcb1ab702a7a0b40c03f388
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-31 18:37:08 -0300

    style: format main class

--- src/main/java/pb/com/Backend_PB/BackendPbApplication.java
+++ src/main/java/pb/com/Backend_PB/BackendPbApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendPbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendPbApplication.class, args);
-		System.out.println("Server is running on http://localhost:8080 ");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPbApplication.class, args);
+        System.out.println("Server is running on http://localhost:8080 ");
+    }
 
 }
